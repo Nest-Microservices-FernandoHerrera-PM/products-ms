31c72e4d1ff0d334d933940807439889c915e66f
pietrodeveloper <devpietro2024@gmail.com>
2026-06-25 13:07:09 -0500

    First commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,100 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma.service';
+import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.prisma.product.create({
+      data: createProductDto,
+    })
+
+    return product;
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page = 1, limit = 10 } = paginationDto;
+    const totalPages = await this.prisma.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.prisma.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.prisma.product.findFirst({
+      where: { id, available: true },
+    })
+
+    if (!product) {
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST
+      });
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const {id: ___, ...data} = updateProductDto
+
+    await this.findOne(id);
+
+    return await this.prisma.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    // return this.prisma.product.delete({
+    //   where: { id },
+    // });
+
+    const product = await this.prisma.product.update({
+      where: { id },
+      data: { available: false },
+    });
+
+    return product;
+  }
+
+  async validateProducts(ids: number[]){
+    ids = Array.from(new Set(ids));
+
+    const products = await this.prisma.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    })
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      })
+    }
+
+    return products;
+  }
+}
